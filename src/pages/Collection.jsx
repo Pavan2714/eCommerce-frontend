7e683fe6d599994a7209a073b28d82491be607b8
27pavann <stud.2201201098@kpgu.ac.in>
2025-07-19 17:29:07 +0530

    added

--- src/pages/Collection.jsx
+++ src/pages/Collection.jsx
@@ -116,7 +116,7 @@ const Collection = () => {
           {/* Product Sort */}
           <select
             onChange={(e) => setSortType(e.target.value)}
-            className="border-2 border-gray-300 text-sm px-2 py-2 rounded w-32 sm:w-40"
+            className="border-2 border-gray-300 text-sm px-2 py-1 rounded w-32 sm:w-40"
           >
             <option value="relavent">Sort by: Relevant</option>
             <option value="low-high">Sort by: Low to High</option>
